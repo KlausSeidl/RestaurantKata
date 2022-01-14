b02ff128e1519a24c5c579580616ea3e771267de
Klaus Seidl <kseidl@recom.eu>
2022-01-14 13:22:24 +0100

    Created projects and initial Class

---
+++ RestaurantService/RestaurantService/TableReservationService.cs
@@ -0,0 +1,21 @@
+﻿namespace RestaurantService
+{
+    public class TableReservationService
+    {
+        public BookTableResponse BookTable()
+        {
+            return new BookTableResponse() { Status = BookTableStatus.NoFreeTable };
+        }
+    }
+
+    public enum BookTableStatus
+    {
+        Success,
+        NoFreeTable
+    }
+    
+    public class BookTableResponse
+    {
+        public BookTableStatus Status { get; set; }    
+    }
+}
---
+++ RestaurantService/RestaurantServiceTests/TableReservationServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RestaurantService;
+
+namespace RestaurantServiceTests
+{
+    [TestFixture]
+    public class TableReservationServiceTests
+    {
+        private TableReservationService _testClass;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _testClass = new TableReservationService();
+        }
+
+        [Test]
+        public void BookTable_with_no_free_table_should_return_NoFreeTable()
+        {
+            // Arrange
+            // --
+            
+            // Act
+            var result = _testClass.BookTable();
+            
+            // Assert
+            result.Status.Should().Be(BookTableStatus.NoFreeTable);
+        }
+    }
+}
