964410df9bc57aa97f9af5fc1e9ddd01351e5daf
Klaus Seidl <kseidl@recom.eu>
2022-01-14 14:31:38 +0100

    Red - Initial test for TableReservationService

---
+++ RestaurantService/RestaurantServiceTests/TableReservationServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace RestaurantServiceTests
+{
+    [TestFixture]
+    public class TableReservationServiceTests
+    {
+        private TableReservationService _testClass;
+        
+        public void Setup()
+        {
+            _testClass = new TableReservationService();
+        }
+    }
+}
